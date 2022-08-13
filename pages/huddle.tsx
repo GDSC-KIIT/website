52e8019bc31632a202070c5bd1f5f90eb6a3a34b
Junaid Rahim <junaidrahim8d@gmail.com>
2022-08-14 00:59:33 +0530

    fix: auto adjust embed height

--- pages/huddle.tsx
+++ pages/huddle.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Script from "next/script";
 
 import Navbar from "../src/common/Navbar";
 import Footer from "../src/common/Footer";
@@ -22,11 +23,13 @@ const Home: NextPage = () => {
 
       <br></br>
       <br></br>
-
+      <Script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></Script>
       <iframe
+        class="airtable-embed airtable-dynamic-height"
         src="https://airtable.com/embed/shrj3RJG3ytCQfDzr?backgroundColor=red"
         width="100%"
         height="1774"
+        style={{ background: "transparent", border: "1px solid #ccc" }}
       ></iframe>
 
       <ContactUs></ContactUs>

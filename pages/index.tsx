8a2ef77fd7d64380dd08fa20682b90c03f9ccebf
Aakash <aakashkumargorai@gmail.com>
2022-09-05 01:37:25 +0530

    Add Chatbot in GDSC KIIT Website

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Script from "next/script";
 
 import Navbar from "../src/common/Navbar";
 import Footer from "../src/common/Footer";
@@ -21,7 +22,12 @@ const Home: NextPage = () => {
         <meta name="viewport" content="width=width-device,initial-scale=1.0"/>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w, d) { w.CollectId = "631465d7f9bc0c46b6db599b"; var h = d.head || d.getElementsByTagName("head")[0]; var s = d.createElement("script"); s.setAttribute("type", "text/javascript"); s.async=true; s.setAttribute("src", "https://collectcdn.com/launcher.js"); h.appendChild(s); })(window, document);`,
+        }}
+      />
       <Navbar></Navbar>
       <Landing></Landing>
       <GetInvolved></GetInvolved>
